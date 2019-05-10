c2eac736c6de39ce003b64e329ff10653a9151c0
Ricll <ricllima@hotmail.com>
2019-05-10 18:38:02 -0300

    User validator

---
+++ src/app/validators/User.js
@@ -0,0 +1,15 @@
+// Validação do Cadastro do Usuário
+
+const Joi = require('joi')
+
+module.exports = {
+  body: {
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email()
+      .required(),
+    password: Joi.string()
+      .required()
+      .min(6)
+  }
+}
