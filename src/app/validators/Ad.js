e96c3af00fd78726e9a949b2b80b6a1475f10679
Ricll <ricllima@hotmail.com>
2019-05-10 18:35:29 -0300

    Ad validator

---
+++ src/app/validators/Ad.js
@@ -0,0 +1,11 @@
+// Validação do Anúncio
+
+const Joi = require('joi')
+
+module.exports = {
+  body: {
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().required()
+  }
+}
