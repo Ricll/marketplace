6dee5b72cdf2e517aa6ad2d9a0677ca218c693a6
Ricll <ricllima@hotmail.com>
2019-05-11 14:05:36 -0300

    Regis and Sentry queue

--- src/app/services/Queue.js
+++ src/app/services/Queue.js
@@ -1,10 +1,13 @@
-// Importa as configuração do servidor Regis
+// // Fila - Importa as configuração do servidor Regis
 // e pega a key e o handle do PurchaseMail
 const kue = require('kue')
+const Sentry = require('@sentry/node')
 const redisConfig = require('../../config/redis')
 const jobs = require('../jobs')
 
 const Queue = kue.createQueue({ redis: redisConfig })
+
 Queue.process(jobs.PurchaseMail.key, jobs.PurchaseMail.handle)
 
+Queue.on('error', Sentry.captureException)
 module.exports = Queue
