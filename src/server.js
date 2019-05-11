f280ac4eca57b008f33a80b1c49caceaa959c75c
Ricll <ricllima@hotmail.com>
2019-05-11 13:55:36 -0300

    server

--- src/server.js
+++ src/server.js
@@ -2,19 +2,26 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
-const databaseConfig = require('./config/database')
-const validate = require('express-validation')
 const Youch = require('youch')
+const Sentry = require('@sentry/node')
+const databaseConfig = require('./config/database')
+const sentryConfig = require('./config/sentry')
+const validate = require('express-validation')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.sentry()
     this.database()
     this.middlewares()
     this.routes()
     this.exception()
+  }
+
+  sentry () {
+    Sentry.init(sentryConfig)
   }
 
   database () {
@@ -26,6 +33,7 @@ class App {
   }
 
   middlewares () {
+    this.express.use(Sentry.Handlers.requestHandler())
     this.express.use(express.json())
   }
 
@@ -33,6 +41,10 @@ class App {
     this.express.use(require('./routes'))
   }
   exception () {
+    if (process.env.NODE_ENV === 'production') {
+      this.express.use(Sentry.Handlers.errorHandler())
+    }
+
     this.express.use(async (err, req, res, next) => {
       if (err instanceof validate.ValidationError) {
         return res.status(err.status).json(err)
